2913fc29fbd04861fa1af13311740a1fd86f2707
Yonaka11 <149323809+Yonaka11@users.noreply.github.com>
2026-08-17 15:55:19 -0400

    expand familiar animation hooks

--- app/src/main/kotlin/com/mikazuki/pocketfamiliar/model/PetProfile.kt
+++ app/src/main/kotlin/com/mikazuki/pocketfamiliar/model/PetProfile.kt
@@ -19,6 +19,16 @@ data class PetProfile(
     val jumpAnim: PetAnimation,
     val holdAnim: PetAnimation = fallAnim,
     val throwAnim: PetAnimation = jumpAnim,
+    val hardLandAnim: PetAnimation = fallAnim,
+    val recoverAnim: PetAnimation = idleAnim,
+    val eatAnim: PetAnimation = idleAnim,
+    val groomAnim: PetAnimation = idleAnim,
+    val happyAnim: PetAnimation = idleAnim,
+    val musicAnim: PetAnimation = idleAnim,
+    val stepAnim: PetAnimation = runAnim,
+    val chargingAnim: PetAnimation = sleepAnim,
+    val lowBatteryAnim: PetAnimation = sleepAnim,
+    val deepSleepAnim: PetAnimation = sleepAnim,
     val physics: FamiliarPhysicsProfile = FamiliarPhysicsProfile(),
 ) {
     fun animationForState(state: PetState): PetAnimation = when (state) {
@@ -28,9 +38,19 @@ data class PetProfile(
         is PetState.RunLeft,
         is PetState.RunRight -> runAnim
         is PetState.Sleep -> sleepAnim
+        is PetState.DeepSleep -> deepSleepAnim
         is PetState.Falling -> fallAnim
         is PetState.Held -> holdAnim
         is PetState.Thrown -> throwAnim
+        is PetState.HardLanding -> hardLandAnim
+        is PetState.Recovering -> recoverAnim
+        is PetState.Eating -> eatAnim
+        is PetState.Grooming -> groomAnim
+        is PetState.Happy -> happyAnim
+        is PetState.Music -> musicAnim
+        is PetState.StepActivity -> stepAnim
+        is PetState.Charging -> chargingAnim
+        is PetState.LowBattery -> lowBatteryAnim
         is PetState.ClimbLeft,
         is PetState.ClimbRight -> climbAnim
         is PetState.Jumping -> jumpAnim
